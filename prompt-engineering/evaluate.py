ec4a52ca3c9af4e89fb13921e9a1c20a2548b473
Daenerys Targaryen <gisela.torres@returngis.net>
2025-04-17 08:01:40 +0000

    Refactor evaluator to support environment variable configurations and add new prompt for YouTube title generation

--- prompt-engineering/evaluate.py
+++ prompt-engineering/evaluate.py
@@ -12,11 +12,29 @@ from rich.table import Table
 load_dotenv()
 
 
-def run_evaluator(evaluator_path, new_title, description):
+def run_evaluator(evaluator_path, new_title, description, ollama=True):
     """
     Run a prompty evaluator and return the score and explanation.
     """
     try:
+
+        if ollama:
+            # Load the configuration from environment variables
+            configuration = {
+                "name": os.getenv('OLLAMA_MODEL'),
+                "type": "openai",
+                "base_url": os.getenv('OLLAMA_API_URL'),
+                "api_key": os.getenv('OLLAMA_API_KEY')
+            }
+        else:
+            # Load the configuration from environment variables for Github Models
+            configuration = {
+                "name": os.getenv('GITHUB_MODELS_MODEL'),
+                "type": "openai",
+                "base_url": os.getenv('GITHUB_MODELS_API_URL'),
+                "api_key": os.getenv('GITHUB_MODELS_API_KEY')
+            }
+
         # Run the evaluator using prompty as a library
         evaluator_file = str(evaluator_path)
 
@@ -24,12 +42,7 @@ def run_evaluator(evaluator_path, new_title, description):
                                  inputs={
                                      "generated_title": new_title, "video_description": description
                                  },
-                                 configuration={
-                                     "name": os.getenv('OLLAMA_MODEL'),
-                                     "type": "openai",
-                                     "base_url": os.getenv('OLLAMA_API_URL'),
-                                     "api_key": os.getenv('OLLAMA_API_KEY')
-                                 })
+                                 configuration=configuration)
 
         # Process the result based on type
         if isinstance(result, dict):
@@ -63,9 +76,11 @@ def run_evaluator(evaluator_path, new_title, description):
         }
 
 
-def main():
+def main(ollama=True):
 
     description = "¡Hola developer! 👋🏻 Aquí tienes el segundo vídeo de mi serie sobre IA Generativa para developers. En él nos metemos de lleno en el código, trabajando con uno de los escenarios más comunes: la generación de texto ✍️. Te mostraré cómo llamar a diferentes modelos en modo stream y no-stream, utilizando SDKs como Mistral y OpenAI. Además, veremos una aplicación de ejemplo que te enseñará cómo integrar estos modelos en el frontend, visualizando los resultados que llegan desde una API conectada con GitHub Models y Ollama 🚀."
+
+    print(f"Starting with Ollama 🦙")
 
     start_time = time.time()
 
@@ -74,7 +89,7 @@ def main():
 
     # Get the new title
     new_title = prompty.execute(
-        Path.cwd() / "prompt-engineering/ollama.prompty",
+        Path.cwd() / "prompt-engineering/generate_title.prompty",
         inputs={"description": description},
         configuration={
             "name": os.getenv('OLLAMA_MODEL_FOR_TEXT_GENERATION'),
@@ -118,7 +133,8 @@ def main():
     for evaluator_file in evaluator_files:
         print(f"Running evaluator: {evaluator_file.name}")
         start_time = time.time()
-        result = run_evaluator(evaluator_file, new_title, description)
+        result = run_evaluator(evaluator_file, new_title,
+                               description, ollama=ollama)
         end_time = time.time()
 
         elapsed_time = end_time - start_time
@@ -148,6 +164,81 @@ def main():
     print(
         f"\nTotal time for all evaluations: {total_elapsed_time / 60:.2f} minutes 🕒")
 
+    print(f"Now let's run the same with Github Models 🚀")
+    start_time = time.time()
+
+    print(
+        f"[bold blue]Model for title generation 📝[/bold blue]: {os.getenv('GITHUB_MODELS_MODEL_FOR_TEXT_GENERATION')}")
+    # Get the new title
+    new_title = prompty.execute(
+        Path.cwd() / "prompt-engineering/generate_title.prompty",
+        inputs={"description": description},
+        configuration={
+            "name": os.getenv('GITHUB_MODELS_MODEL_FOR_TEXT_GENERATION'),
+            "type": "openai",
+            "base_url": os.getenv('GITHUB_MODELS_API_URL'),
+            "api_key": os.getenv('GITHUB_MODELS_API_KEY')
+        }
+    )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Generated title ✨: {new_title}")
+    print(f"Title generation executed in {elapsed_time / 60:.2f} minutes 🕒.")
+
+    print(
+        f"Evaluator using [bold green]{os.getenv('GITHUB_MODELS_MODEL')}[/bold green]")
+
+    # Find the evaluators directory
+    evaluators_dir = Path.cwd() / 'prompt-engineering/llm-as-a-judge'
+    if not evaluators_dir.exists() or not evaluators_dir.is_dir():
+        print(f"Error: '{evaluators_dir}' directory not found.")
+        sys.exit(1)
+    # Find all evaluator files in the directory
+    evaluator_files = [f for f in evaluators_dir.iterdir(
+    ) if f.is_file() and f.suffix == '.prompty']
+    if not evaluator_files:
+        print(f"Error: No evaluator files found in '{evaluators_dir}'.")
+        sys.exit(1)
+    print(f"Found {len(evaluator_files)} evaluators in '{evaluators_dir}'.")
+    # Track total elapsed time for all evaluators
+    total_elapsed_time = 0
+    # Run each evaluator and collect results
+    results = []
+    for evaluator_path in evaluator_files:
+        print(f"Running evaluator: {evaluator_path.name}")
+        start_time = time.time()
+        result = run_evaluator(evaluator_path, new_title,
+                               description, ollama=False)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        total_elapsed_time += elapsed_time
+        print(
+            f"Evaluator '{evaluator_path.name}' executed in {elapsed_time / 60:.2f} minutes 🕒"
+        )
+
+        results.append(result)
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Evaluator", style="dim")
+    table.add_column("Score")
+    table.add_column("Explanation")
+    for result in results:
+        score = int(result['score']) if str(result['score']).isdigit() else 0
+        score_color = "green" if score == 5 else "yellow" if score in [
+            3, 4] else "red"
+        table.add_row(
+            result['evaluator'],
+            f"[bold {score_color}]{result['score']}[/bold {score_color}]",
+            str(result['explanation'])
+        )
+    print(table)
+    # Display total elapsed time
+    print(
+        f"\nTotal time for all evaluations: {total_elapsed_time / 60:.2f} minutes 🕒"
+
+    )
+    print("Evaluation process completed. Thank you for using the evaluator! 🚀")
+
 
 if __name__ == "__main__":
-    main()
+    # Pass True for Ollama or False for GitHub Models
+    main(ollama=True)
